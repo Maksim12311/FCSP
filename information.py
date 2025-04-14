ab7beaf5d283a683ef3951549b118514e66d56fd
Maksim Brusilovskii <145996549+Maksim12311@users.noreply.github.com>
2025-04-14 15:01:26 +0300

    second commit

--- information.py
+++ information.py
@@ -2,14 +2,26 @@ def add_student(name, surname, date_of_birth, area_of_study, citizenship):
     with open("students.txt", "a") as f:
         f.write(f"{name},{surname},{date_of_birth},{area_of_study},{citizenship}\n")
 
+
+
 def change_information(name, new_data):
     with open("students.txt", "r") as f:
         students = f.readlines()
 
-    
     with open("students.txt", "w") as f:
         for student in students:
             if student.startswith(name):
                 f.write(new_data + "\n")
             else:
+                f.write(student)
+
+
+
+def delete_student(name):
+    with open("students.txt", "r") as f:
+        students = f.readlines()
+
+    with open("students.txt", "w") as f:
+        for student in students:
+            if not student.startswith(name):
                 f.write(student)
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from .information import add_student, change_information
+from .information import add_student, change_information, delete_student
 
 add_student("Ivan", "Ivanov", "2000-01-01", "IT", "Russia")
 change_information("Ivan", "Ivanov", "2000-01-01","Data Science","Russia")
