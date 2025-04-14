afb17efea1af74f4ffccbc09924f77ad5e2d05f3
Maksim Brusilovskii <145996549+Maksim12311@users.noreply.github.com>
2025-04-14 14:41:14 +0300

    First commit

---
+++ information.py
@@ -0,0 +1,15 @@
+def add_student(name, surname, date_of_birth, area_of_study, citizenship):
+    with open("students.txt", "a") as f:
+        f.write(f"{name},{surname},{date_of_birth},{area_of_study},{citizenship}\n")
+
+def change_information(name, new_data):
+    with open("students.txt", "r") as f:
+        students = f.readlines()
+
+    
+    with open("students.txt", "w") as f:
+        for student in students:
+            if student.startswith(name):
+                f.write(new_data + "\n")
+            else:
+                f.write(student)
